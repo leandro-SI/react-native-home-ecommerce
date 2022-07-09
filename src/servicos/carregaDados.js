e679689ba5d1f61bda600b531785ee267733f32a
Leandro <leandro.almeida@uvvnet.com.br>
2022-07-09 19:14:36 -0300

    criando mocks e serviços

---
+++ src/servicos/carregaDados.js
@@ -0,0 +1,12 @@
+import produtores from '../mocks/produtores';
+import topo from '../mocks/topo';
+
+
+export const carregaProdutores = () => {
+    return produtores;
+}
+
+
+export const carregaTopo = () => {
+    return topo;
+}
